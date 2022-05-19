0a42c2ab5969c6808e873be97640eea0b24adf37
518651 <69994181+518651@users.noreply.github.com>
2022-05-19 23:34:36 +0800

    Update tray.js

--- src/electron/tray.js
+++ src/electron/tray.js
@@ -197,7 +197,7 @@ export function createTray(win, eventEmitter) {
     });
 
   let tray = new Tray(icon);
-  tray.setToolTip('YesPlayMusic');
+  tray.setToolTip('The 7E9FB-保持热爱，奔赴星辰大海');
 
   return isLinux
     ? new YPMTrayLinuxImpl(tray, win, eventEmitter)
